f6f14bb5ad60d38b202d4cd507a070ab4041e0a6
h0nda <41300123+h0nde@users.noreply.github.com>
2020-11-12 16:04:27 +0100

    Update proxied_token_generator.py

--- examples/proxied_token_generator.py
+++ examples/proxied_token_generator.py
@@ -4,7 +4,7 @@ import itertools
 
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
 output_file = open("tokens.txt", "a")
-workers = 100
+workers = 10
 
 with open("proxies.txt", encoding="UTF-8", errors="ignore") as f:
     proxies = f.read().splitlines()
